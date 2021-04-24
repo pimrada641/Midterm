35cea57a5e659301a9f4fc4442601bb2c6b84c41
Pimrada641 <pimrada641@hotmail.com>
2021-04-24 15:23:11 +0700

    added file

---
+++ 6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String character = Console.ReadLine();
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int amount = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= amount; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    for(int k = 0; k < width*i; k++)
+                    {
+                        Console.Write(character);
+                    }
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
