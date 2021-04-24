85742baa8e1888efc9832d3968202441513b7e28
Pimrada641 <pimrada641@hotmail.com>
2021-04-24 16:03:25 +0700

    added

---
+++ 7.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            string opa = Console.ReadLine();
+            while (opa == "+" || || opa == "-" || opa == "*" || opa == "/" || opa == "^"|| opa == "|")
+            {
+            if (opa == "+" || opa == "-" || opa == "*" || opa == "/") 
+            { 
+                int c = int.Parse(Console.ReadLine());
+                int d = int.Parse(Console.ReadLine());
+                switch (opa)
+                {
+                    case "+":
+                        (a+bi)+(c+di) = (a+c)+(b+d)i
+                        break;
+                    case "-":
+                        (a + bi) - (c + di) = (a - c) + (b - d)i
+                        break;
+                    case "*":
+                        (a + bi) * (c + di) = (ac + cd) + (ad + bc)i
+                        break;
+                    case "/":
+                        (a + bi) / (c + di) = ((ac + bd) / (Math.Pow(c,2)) + (Math.Pow(d, 2)) + (ad + bc)i
+                        break;
+                }
+            }
+            else if (opa == "^")
+            {
+                int numtoexponent = int.Parse(Console.ReadLine());
+            }
+            else if (opa == "|")
+            {
+
+            }
+            }
+        }
+    }
+}
